2e1e2c84f98515e755050608399f802972922bbc
Rob <robdawson123.rd@gmail.com>
2024-08-07 15:12:04 +0100

    Changed file names from h to hpp

--- bumperbot_examples_cpp/include/turtlesim_kinematics.hpp
+++ bumperbot_examples_cpp/include/turtlesim_kinematics.hpp
@@ -1,7 +1,19 @@
 #include <rclcpp/rclcpp.hpp>
-
-class TurtlesimKinematics : public rclcpp::Node
+#include <turtlesim/msg/pose.hpp>
+class TurtleSimKinematics : public rclcpp::Node
 {
+public:
+    TurtleSimKinematics(const std::string &name);
 
+private:
+    // rclcpp::Publisher<geometry_msgs::msg::Twist>::SharedPtr pub;
+
+    void turtle1PoseCallback(const turtlesim::msg::Pose &pose1);
+    void turtle2PoseCallback(const turtlesim::msg::Pose &pose2);
+    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subTurtle1;
+    rclcpp::Subscription<turtlesim::msg::Pose>::SharedPtr subTurtle2;
+
+    turtlesim::msg::Pose lastTurtle1Pose;
+    turtlesim::msg::Pose lastTurtle2Pose;
 };
 
